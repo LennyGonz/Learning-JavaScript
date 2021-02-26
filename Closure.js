ad4466cccf34f973ed5bf9b3fca0cebe45fc1a6d
lenny <lenny.gonzaleznyc@gmail.com>
2021-02-25 23:09:05 -0500

    Adding Interview Questions on Closures

--- Closure.js
+++ Closure.js
@@ -25,3 +25,36 @@ console.log(chocolateExpert(1000));
 console.log(strawberryExpert(500));
 // 500 strawberry candies are made by Alex
 
+// ***********************************
+// 7 Questions on Javascript Closures
+// ***********************************
+
+
+// 1. Consider the following functions clickHandler(), immediate(), and delayedReload()
+let countClicks = 0;
+button.addEventListener('click', function clickHandler() {
+  countClicks++;
+});
+
+const result = (function immediate(number) {
+  const message = `number is: $(number)`;
+  return message;
+})(100);
+
+setTimeout(function delayedReload() {
+  location.reload();
+}, 1000);
+
+// Which of these 3 functions access outer scope variables?
+/**
+ * clickHandler() if you create an exection context
+ * local memory for this function has no declaration of a variable called countClicks
+ * so we look for this variable in the Global memory, which is where countClicks is initialized
+ * SO - yes clickHandler() has access to outer scope variables
+ * 
+ * 
+ * immediate() doesn't access any variables in the outer scope
+ * 
+ * delayedReload() access global variable location - location is not a user defined variable, but it's a browser object.
+ * So it will always be accessible in the global scope
+ */
